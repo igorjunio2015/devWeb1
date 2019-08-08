32af1114a978ef06d3553bbee5dce5cc82a07d5e
Igor Junio <53827311+igorjunio2015@users.noreply.github.com>
2019-08-07 21:53:47 -0300

    Add files via upload

--- principal.java
+++ principal.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.StringBuilder;
 
 public class principal extends pessoa{
     public static void main(String[] args){
@@ -8,7 +9,19 @@ public class principal extends pessoa{
         pessoas.setNome(input.nextLine());
         pessoas.setIdade(Integer.parseInt(input.nextLine()));
 
-        System.out.print("\nNome: " + pessoas.getNome() + ", idade: " + pessoas.getIdade() + " anos, ");
+        pessoa pessoasNew = new pessoa();
+        pessoasNew.setNome(input.nextLine());
+        pessoasNew.setIdade(Integer.parseInt(input.nextLine()));
+
+        StringBuilder nome1 = new StringBuilder();
+        StringBuilder nome2 = new StringBuilder();
+        nome1.append("\nNome: ").append(pessoas.getNome()).append(", idade: ").append(pessoas.getIdade()).append(" anos, ");
+        nome2.append("\nNome: ").append(pessoasNew.getNome()).append(", idade: ").append(pessoasNew.getIdade()).append(" anos, ");
+
+
+        System.out.print(nome1.toString());
         pessoas.falar();
+        System.out.print(nome2.toString());
+        pessoasNew.falar();
     }
 }
